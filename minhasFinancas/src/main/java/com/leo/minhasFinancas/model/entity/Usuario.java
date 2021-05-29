9ebae3e815aa16c9e06a4b02e7fb9a8e87fec20a
leonardocosta <sir.costa@yahoo.com.br>
2021-05-29 10:42:22 -0300

    Adição do Lombok ao projeto

--- minhasFinancas/src/main/java/com/leo/minhasFinancas/model/entity/Usuario.java
+++ minhasFinancas/src/main/java/com/leo/minhasFinancas/model/entity/Usuario.java
@@ -1,12 +1,15 @@
 package com.leo.minhasFinancas.model.entity;
 
+import lombok.*;
+
 import javax.persistence.*;
-import java.util.Objects;
 
 @Entity
 @Table(name = "usuario", schema = "financas")
+@Builder
+@Data
 public class Usuario {
-
+    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
@@ -23,56 +26,5 @@ public class Usuario {
 
     public Long getId() {
         return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getSenha() {
-        return senha;
-    }
-
-    public void setSenha(String senha) {
-        this.senha = senha;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Usuario usuario = (Usuario) o;
-        return Objects.equals(id, usuario.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
-
-    @Override
-    public String toString() {
-        return "Usuario{" +
-                "id=" + id +
-                ", nome='" + nome + '\'' +
-                ", email='" + email + '\'' +
-                ", senha='" + senha + '\'' +
-                '}';
     }
 }
