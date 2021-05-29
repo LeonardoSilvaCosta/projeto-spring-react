f0153dccb6b62eabc13273878f6281420e06b1d9
leonardocosta <sir.costa@yahoo.com.br>
2021-05-29 09:58:48 -0300

    Criacao e mapeamento  das entidades Usuario e Lancamentos

---
+++ minhasFinancas/src/main/java/com/leo/minhasFinancas/model/entity/Lancamento.java
@@ -0,0 +1,35 @@
+package com.leo.minhasFinancas.model.entity;
+
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "lancamento", schema = "financas")
+public class Lancamento {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+     private Long id;
+
+    @Column(name = "mes")
+    private Integer mes;
+
+    @Column(name = "ano")
+    private Integer ano;
+
+    @ManyToOne
+    @JoinColumn(name = "id_usuario")
+    private Usuario usuario;
+
+    @Column(name = "valor")
+    private BigDecimal valor;
+
+    @Column(name = "data_cadastro")
+    @Convert(converter = Jsr310JpaConverters.LocalDateConverter.class)
+    private LocalDate dataCadastro;
+}
